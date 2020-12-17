e5469f85b445be386fdd8eb2fc610a2473b0c3fb
Jin-devST <kpasd002@gmail.com>
2020-12-17 13:27:19 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,72 +1,24 @@
 import React from "react";
-import Axois from "axios";
-import Movie from "./Movie";
-import "./App.css";
-import Axios from "axios";
-// import PropTypes from "prop-types";
-/**
- * 
- *    React - state
- *    state 를 사용하지 않을 때는 function 으로 선언해도 무관하다. 사용하면 class 로 선언 후 React.Component 를 상속받는다.
- */
-class App extends React.Component {
-  /*
-  state = {
-    count: 0
-  };
-  add = () => {
-    //this.setState({count : this.state.count + 1});
-    this.setState(current => ({count : current.count + 1})); // 이쪽이 좀더 좋은 표현
-  };
-  */
- state = {
-   isLoading : true,
-   movies: []
- };
+import {HashRouter, Route} from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
- getMovies = async () => {
-  const {
-    data: {
-      data: { movies }
-    }
-  } = await Axios.get(
-    "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-  );
-
-  //console.log(movie.data.data.movies);
-  this.setState({movies, isLoading : false});
- }
- componentDidMount(){
-  this.getMovies();
- };
- 
- render() {
-  const {isLoading , movies} = this.state;
+function App(params) {
   return (
-    <section className="container">
-      {isLoading ? (
-        <div className="loader">
-          <span className="loader__text">Loading ...</span>
-        </div>
-      ) : (
-        <div className="movies">
-          {movies.map(movie => (
-                     <Movie
-                     key={movie.id}
-                     id={movie.id}
-                     year={movie.year}
-                     title={movie.title}
-                     summary={movie.summary.slice(0,100)}
-                     poster={movie.medium_cover_image}
-                     genres={movie.genres}
-                     />
-          ))}
-        </div>
-      )
-      }
-    </section>
-  );
- }
+    <HashRouter>
+      <Route path="/" exact={true} component={Home}>
+        {/* <h1>Home</h1> */}
+      </Route>
+      <Route path="/about" component={About}>
+        {/* <h1>About</h1> */}
+      </Route>
+      {/* <Route path="/introduction" component={Introduction}> */}
+        {/* <h1>Introduction</h1> */}
+      {/* </Route> */}
+    </HashRouter>
+    );
 }
- 
+
+
 export default App;
+
